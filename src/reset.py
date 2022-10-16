efb3ac8a8fba2c948c414afed68f03101f518d36
pokir <pokir@users.noreply.github.com>
2022-10-16 22:43:02 +0200

    Add automatic resetting

---
+++ src/reset.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# From: https://stackoverflow.com/a/49384222
+
+import sys
+import serial
+
+
+com = serial.Serial(sys.argv[1], 1200)
+com.close()
+
+# To use (while loop from https://stackoverflow.com/a/46040407):
+```
+python3 ./reset.py "/dev/ttyACM0"
+while :; do
+  sleep 0.5
+  [ -c "${ARDUINO_UPLOAD_PORT}" ] && break
+done
+```
+
+# Then run avrdude
