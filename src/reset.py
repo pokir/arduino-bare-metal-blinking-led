741f1064163ccc6e53c08d3fa9828f0d668bfc66
pokir <pokir@users.noreply.github.com>
2022-10-16 22:46:10 +0200

    Fix multiline comment syntax

--- src/reset.py
+++ src/reset.py
@@ -10,12 +10,12 @@ com = serial.Serial(sys.argv[1], 1200)
 com.close()
 
 # To use (while loop from https://stackoverflow.com/a/46040407):
-```
+'''
 python3 ./reset.py "/dev/ttyACM0"
 while :; do
   sleep 0.5
   [ -c "${ARDUINO_UPLOAD_PORT}" ] && break
 done
-```
+'''
 
 # Then run avrdude
